b7817535e5f8bec95104a04a65518d4bb32f7009
ThomasBreuer <sam@math.rwth-aachen.de>
2020-05-09 13:59:42 +0200

    version with Jupyter notebook
    - fixed the bug in `less_or_equal_array_bitlist`
      which caused that too many hashes were collected in `fan_traversal`
    
    - extended the example code (compute the Polymake fan object,
      compute the incidence graph of the orbits, compute the F-vector)
    
    - added a first Jupyter notebook showing the example

--- example.jl
+++ example.jl
@@ -1,7 +1,8 @@
 ############################################################################
-##  The computations shown here belong to Example 5.2 in the paper at
-##  https://arxiv.org/abs/1603.09241
-##  (Computing GIT-Fans with Symmetry and the Mori Chamber Decomposition ...)
+##  The computations shown here belong to Example 5.2 in the following paper:
+##  [J. Boehm, S. Keicher, Y. Ren:
+##  Computing GIT-Fans with Symmetry and the Mori Chamber Decomposition of
+##  $\bar{M}_{0,6}$](https://arxiv.org/abs/1603.09241)
 
 using GITFans
 using GAP
@@ -10,7 +11,7 @@ using Singular
 
 
 ############################################################################
-##  1. the input data (see the paper)
+##  Enter the input as described in Example 5.2 of the paper.
 
 # grading matrix
 Q = [
@@ -41,25 +42,49 @@ II = Singular.Ideal( R, [
 # symmetries: (2,3)(5,6)(9,10), (1,5,9,10,3)(2,7,8,4,6)
 perms_list = [ [1,3,2,4,6,5,7,8,10,9], [5,7,1,6,9,2,8,4,10,3] ];
 perms = [ GAP.Globals.PermList(GAP.julia_to_gap(i)) for i in perms_list ];
-G = GAP.Globals.Group( GAP.julia_to_gap( perms ) )
+G = GAP.Globals.Group(GAP.julia_to_gap(perms))
 
 
 ############################################################################
-##  2. preprocessing: compute G-orbits and induced action
+##  Preprocessing:
+##  Encode the GIT fan by the array of orbit cone orbits,
+##  the array of homomorphism objects describing the induced actions
+##  of G on the orbits,
+##  and the matrix Q.
 
-fan_descr = GITFans.GITFan( II, Q, G );
-
-
-############################################################################
-##  3. fan traversal: compute neighbors until the list is complete
-
-hash_list = GITFans.fan_traversal( fan_descr, Q );
-
-length( hash_list )
+fan_descr = GITFans.GITFan(II, Q, G);
 
 
 ############################################################################
-##  4. translate the hashes back to cones
+##  Execute the fan traversal,
+##  that is, compute neighbors of the known cones,
+##  and then the smallest representatives in the G-orbits;
+##  each cone is encoded by the set of (indices of) maximal cones
+##  containing it.
+##  At the same time,
+##  also the incidence relation for the orbits is computed.
+
+(hash_list, edges) = GITFans.fan_traversal(fan_descr);
+
+
+############################################################################
+##  There are six maximal cones, up to G-symmetry.
+
+length(hash_list)
+
+
+############################################################################
+##  We ask Polymake to create the incidence graph of the orbits,
+##  and to visualize it.
+
+intergraph = Polymake.graph.graph_from_edges(collect(edges));
+
+Polymake.graph.visual(intergraph)
+
+
+############################################################################
+##  We translate the descriptions of the six maximal cones back
+##  to cone objects ...
 
 orbit_list = fan_descr[1];
 
@@ -68,41 +93,62 @@ result_cones = map(x -> Polymake.polytope.intersection(
 
 
 ############################################################################
-##  5. expand the orbits (yields 76 maximal cones)
+##  ... and expand their G-orbits
 
-hom = GITFans.action_on_target( Q, G );
-expanded = GITFans.orbit_cone_orbits( result_cones, hom; disjoint_orbits = false )
-length( expanded )              # 6
-map( length, expanded )
-sum( map( length, expanded ) )  # 76
+hom = GITFans.action_on_target(Q, G);
+expanded = GITFans.orbit_cone_orbits(result_cones, hom);
+orbit_lengths = map(length, expanded)
 
 
 ############################################################################
-##  6. create the intersection graph
+##  There are altogether 76 maximal cones.
 
-maxcones = [];
-for orb in expanded
-  append!( maxcones, orb )
-end
+sum(orbit_lengths)
 
-dims = Set( map( Polymake.polytope.dim, maxcones ) )
-codim1 = collect( dims )[1] - 1
 
-edges = []
-for i in 1:length( maxcones )
-  for j in 1:(i-1)
-    if Polymake.polytope.dim(
-           Polymake.polytope.intersection( maxcones[i], maxcones[j] ) ) == codim1
-      push!( edges, [ j, i ] )
-    end
-  end
-end
+############################################################################
+##  The full intersection graph of the fan has (76 vertices and) 180 edges.
+##  (A simpleminded visualization of this graph is not very enlightening.)
 
-length( edges )    # 180
+maxcones = vcat( expanded... );
 
-edges = convert( Vector{Vector{Int}}, edges );
+full_edges = GITFans.edges_intersection_graph(maxcones, first(dims) - 1);
 
-intergraph = Polymake.graph.graph_from_edges( edges );
+length(full_edges)
 
-Polymake.graph.visual( intergraph )
+# full_intergraph = Polymake.graph.graph_from_edges(collect(full_edges));
+
+# Polymake.graph.visual(full_intergraph)
+
+
+############################################################################
+##  We create the fan object in Polymake ...
+
+rays_maxcones = [ [ convert( Vector{Rational{BigInt}}, cone.RAYS[i, :] )
+                    for i in 1:size( cone.RAYS, 1 ) ]
+                  for cone in maxcones ];
+
+allrays = sort( collect( Set( vcat( rays_maxcones... ) ) ) )
+
+index_maxcones = [ sort( [ findfirst( x -> x == v, allrays )-1 for v in rays ] )
+                   for rays in rays_maxcones ]
+
+inputrays = hcat( allrays...)'
+fanobj = Polymake.fan.PolyhedralFan( INPUT_RAYS = inputrays, INPUT_CONES = index_maxcones )
+
+
+############################################################################
+##  ... and compute its F-vector.
+
+fanobj.F_VECTOR
+
+
+############################################################################
+##  We check some statements from the paper.
+##  There are 36 orbit cones of dimension 5, in 4 orbits.
+
+oc = GITFans.orbit_cones( II, Q, G );
+hom = GITFans.action_on_target( Q, G );
+oco = GITFans.orbit_cone_orbits( oc, hom );
+map( length, oco )
 
